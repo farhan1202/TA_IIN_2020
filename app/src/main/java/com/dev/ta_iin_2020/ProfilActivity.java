83657bcd753f62e0a84c0751b9e16dbc0eb675e7
RirinVamori <ririnvanmory27@gmail.com>
2020-08-22 18:00:12 +0700

    new commit

---
+++ app/src/main/java/com/dev/ta_iin_2020/ProfilActivity.java
@@ -0,0 +1,17 @@
+package com.dev.ta_iin_2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+        getSupportActionBar().setTitle("Profile");
+
+
+    }
+}
